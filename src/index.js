8c2aa090e58a4548cf48f39176d46cdb59ebb999
Tatsiana Hladkaya <tatyana.hladkaya@gmail.com>
2024-08-31 20:10:25 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
+    let result = '';
+    let str = n.toString();
+    let i = 0;
+    let a = 1
   
+    while(i < str.length) {
+      result = result + str[str.length-a];
+      i++;
+      a++;
+    }
+  
+    return Number(result);
 }
